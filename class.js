98005749e5ef8a4aba9750b0860ed149563898e3
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-02-11 22:09:26 +0600

    add class output as a object

--- class.js
+++ class.js
@@ -8,3 +8,18 @@ class Vehicle {
         console.log('garee cole na cole ne');
     }
 }
+
+const busGaree = new Vehicle('Pagla Hanif', 10000000)
+console.log(busGaree);
+
+// output
+// Vehicle { name: 'Pagla Hanif', price: 10000000 }
+
+
+class Bus extends Vehicle{
+    constructor(name, price, seat, ticketPrice){
+        super(name, price)
+        this.seat = seat;
+        this.ticketPrice = ticketPrice;
+    }
+}
