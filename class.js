00d70da163d67b819569edde5dbda341e2289fe7
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-02-11 21:16:04 +0600

    create class and add inheritance

---
+++ class.js
@@ -0,0 +1,10 @@
+class Vehicle {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    move(){
+        console.log('garee cole na cole ne');
+    }
+}
--- es6.js
+++ es6.js
@@ -9,3 +9,8 @@ const number = [20, 55, 15]
 
 const result = `His name is ${person.name} , age ${person.age} , he has got ${number[1]} in math exam`
 console.log(result);
+
+
+// arrow function
+
+const sum = (a , b) => a + b
